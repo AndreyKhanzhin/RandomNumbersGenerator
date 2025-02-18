07538a5af25af350881d425cb1e173874368ab1b
AndreyKhanzhin <118679620+AndreyKhanzhin@users.noreply.github.com>
2025-02-18 13:42:16 +0300

    Add files via upload

---
+++ RNG.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Программа сгенерирует случайное число от 1 до 100. У вас есть 5 попыток, чтобы его угадать.");
+Random rnd = new Random();
+int rand = rnd.Next(1, 100);
+Console.WriteLine("Число загадано.");
+int Try = 0;
+while (Try < 5){
+    string count = Console.ReadLine();
+    if (!int.TryParse(count, out int number)){
+        Console.WriteLine("Введите ЧИСЛО.");
+    }
+    else if (number != rand){
+        Console.WriteLine("Неправильно.");
+        Try++;
+    }
+    else{
+        Console.WriteLine("Поздравляем, вы угадали.");
+        break;
+    }
+}
